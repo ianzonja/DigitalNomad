a170473fd302d00676cb55a3d3ee6d0a6b0d15b9
Mihovil <mihmarici@foil.hr>
2017-11-01 15:33:15 +0100

    facebook login - smanjena sdk verzija na 25 jer nisam mogao naci gresku u dependencyima

--- app/src/main/java/com/example/mihovil/digitalnomad/Login_activity.java
+++ app/src/main/java/com/example/mihovil/digitalnomad/Login_activity.java
@@ -3,24 +3,70 @@ package com.example.mihovil.digitalnomad;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.appevents.AppEventsLogger;
+
 import com.example.webservice.interfaces.ServiceResponse;
 import com.example.webservice.interfaces.OnServiceFinished;
 import com.example.webservice.interfaces.WebServiceCaller;
+
+//facebook
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import org.json.JSONObject;
 
 public class Login_activity extends AppCompatActivity implements OnServiceFinished {
     private EditText mail;
     private EditText pass;
 
+    //facebook
+    LoginButton loginButton;
+    CallbackManager callbackManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_activity);
+
+        //facebook
+        loginButton = (LoginButton) findViewById(R.id.login_button);
+      //  loginButton.setReadPermissions("email","public_profile");
+        callbackManager = CallbackManager.Factory.create();
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                loginButton.setVisibility(View.INVISIBLE);
+                startActivity(new Intent(getBaseContext(), MainMenuActivity.class));
+                finish();
+                Log.d("TAG","success");
+            }
+
+            @Override
+            public void onCancel() {
+                Log.d("TAG","cancel");
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Log.d("TAG","error");
+            }
+        });
+
+
+
         mail = (EditText) findViewById(R.id.email);
         pass = (EditText) findViewById(R.id.pass);
         Button login = (Button) findViewById(R.id.button);
@@ -43,6 +89,11 @@ public class Login_activity extends AppCompatActivity implements OnServiceFinish
                 startActivity(new Intent(getBaseContext(), RegistracijaActivity.class));
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode,resultCode,data);
     }
 
     @Override
