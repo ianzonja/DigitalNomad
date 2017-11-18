240fdcacbd2352e4103b3d3747ace9d14b161843
Mihovil <mihmarici@foil.hr>
2017-11-18 17:47:41 +0100

    Korisnicki profil
    Stvoren fragment za mijenjanje lozinke usera i dodani widgeti na fragmentima user_profile_fragment i user_profile_fragment_edit_profile

--- app/src/main/java/com/example/mihovil/digitalnomad/fragments/UserProfileFragment.java
+++ app/src/main/java/com/example/mihovil/digitalnomad/fragments/UserProfileFragment.java
@@ -1,10 +1,26 @@
 package com.example.mihovil.digitalnomad.fragments;
 
 import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by Mihovil on 17.11.2017..
  */
 
 public class UserProfileFragment extends Fragment {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
 }
