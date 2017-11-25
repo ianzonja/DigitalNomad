4b7b09e550ed28ac4ec554701f7c824eaea5c4dc
Mihovil <mihmarici@foil.hr>
2017-11-25 15:05:25 +0100

    Popravljanje zadnjih commitova
    Ponovno ubacena logika za user_profile i reformatiranje koda na main activityu kod rada s prikazom fragmenta u navigation draweru

--- app/src/main/java/com/example/mihovil/digitalnomad/MainMenuActivity.java
+++ app/src/main/java/com/example/mihovil/digitalnomad/MainMenuActivity.java
@@ -10,9 +10,9 @@ import android.preference.PreferenceManager;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 
-import android.app.Fragment;
-import android.app.FragmentManager;
-import android.app.FragmentTransaction;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -34,7 +34,6 @@ import com.facebook.login.LoginManager;
 public class MainMenuActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
-    FragmentManager fragmentMenager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -50,11 +49,6 @@ public class MainMenuActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
-
-        fragmentMenager = getFragmentManager();
-        fragmentMenager.beginTransaction()
-                .replace(R.id.content_frame, new UserProfileFragment())
-                .commit();
 
     }
 
@@ -90,43 +84,39 @@ public class MainMenuActivity extends AppCompatActivity
         return super.onOptionsItemSelected(item);
     }
 
+    private void displaySelectedFragment(int id){
+        Fragment fragment = null;
+
+        switch(id){
+            case R.id.nav_user_profile:
+                fragment = new UserProfileFragment();
+                break;
+            case R.id.nav_workspaces:
+                fragment = new RecyclerViewFragment();
+                break;
+            case R.id.nav_logout:
+                Logout();
+                startActivity(new Intent(getBaseContext(), Login_activity.class));
+                finish();
+        }
+        if (fragment !=null){
+            FragmentTransaction ft =getSupportFragmentManager().beginTransaction();
+            ft.replace(R.id.content_frame,fragment);
+            ft.commit();
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+
+    }
 
 
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        fragmentMenager = getFragmentManager();
+
         int id = item.getItemId();
-
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
-        } else if(id == R.id.nav_user_profile){
-            fragmentMenager.beginTransaction()
-                    .replace(R.id.content_frame, new UserProfileFragment())
-                    .commit();
-
-        } else if(id == R.id.nav_workspaces){
-            fragmentMenager.beginTransaction()
-                    .replace(R.id.content_frame, new RecyclerViewFragment())
-                    .commit();
-        } else if(id == R.id.nav_logout){
-            Logout();
-            startActivity(new Intent(getBaseContext(), Login_activity.class));
-            finish();
-        }
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
+        displaySelectedFragment(id);
         return true;
     }
     private void Logout() {
--- app/src/main/java/com/example/mihovil/digitalnomad/fragments/RecyclerViewFragment.java
+++ app/src/main/java/com/example/mihovil/digitalnomad/fragments/RecyclerViewFragment.java
@@ -1,8 +1,9 @@
 package com.example.mihovil.digitalnomad.fragments;
 
 import android.app.Activity;
-import android.app.Fragment;
+
 import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
