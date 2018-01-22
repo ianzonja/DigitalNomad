f7183bafcabcb5aff4def9d074212236f0b5a706
ianzonja <ianzonja@foi.hr>
2018-01-22 12:23:40 +0100

    Implementirati gallery

---
+++ webservice/src/main/java/com/example/webservice/interfaces/WorkspaceGallery.java
@@ -0,0 +1,17 @@
+package com.example.webservice.interfaces;
+
+/**
+ * Created by Ian on 1/22/2018.
+ */
+
+public class WorkspaceGallery {
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public String path;
+}
